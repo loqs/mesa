ValveSoftware/steamos_mesa

python: Use the print function
In Python 2, `print` was a statement, but it became a function in
Python 3.

Using print functions everywhere makes the script compatible with Python
versions >= 2.6, including Python 3.

Signed-off-by: Mathieu Bridon <bochecha@daitauha.fr>
Acked-by: Eric Engestrom <eric.engestrom@intel.com>
Acked-by: Dylan Baker <dylan@pnwbakers.com>

src/gallium/auxiliary/indices/u_unfilled_gen.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 copyright = '''
 /*
  * Copyright 2009 VMware, Inc.
@@ -53,9 +55,9 @@ outtype_idx = dict(ushort='OUT_USHORT', uint='OUT_UINT')
 
 
 def prolog():
-    print '''/* File automatically generated by u_unfilled_gen.py */'''
-    print copyright
-    print r'''
+    print('''/* File automatically generated by u_unfilled_gen.py */''')
+    print(copyright)
+    print(r'''
 
 /**
  * @file
@@ -93,7 +95,7 @@ static unsigned in_size_idx( unsigned index_size )
 static u_generate_func generate_line[OUT_COUNT][PRIM_COUNT];
 static u_translate_func translate_line[IN_COUNT][OUT_COUNT][PRIM_COUNT];
 
-'''
+''')
 
 def vert( intype, outtype, v0 ):
     if intype == GENERATE:
@@ -102,8 +104,8 @@ def vert( intype, outtype, v0 ):
         return '(' + outtype + ')in[' + v0 + ']'
 
 def line( intype, outtype, ptr, v0, v1 ):
-    print '      (' + ptr + ')[0] = ' + vert( intype, outtype, v0 ) + ';'
-    print '      (' + ptr + ')[1] = ' + vert( intype, outtype, v1 ) + ';'
+    print('      (' + ptr + ')[0] = ' + vert( intype, outtype, v0 ) + ';')
+    print('      (' + ptr + ')[1] = ' + vert( intype, outtype, v1 ) + ';')
 
 # XXX: have the opportunity here to avoid over-drawing shared lines in
 # tristrips, fans, etc, by integrating this into the calling functions
@@ -127,89 +129,89 @@ def name(intype, outtype, prim):
         return 'translate_' + prim + '_' + intype + '2' + outtype
 
 def preamble(intype, outtype, prim):
-    print 'static void ' + name( intype, outtype, prim ) + '('
+    print('static void ' + name( intype, outtype, prim ) + '(')
     if intype != GENERATE:
-        print '    const void * _in,'
-    print '    unsigned start,'
+        print('    const void * _in,')
+    print('    unsigned start,')
     if intype != GENERATE:
-        print '    unsigned in_nr,'
-    print '    unsigned out_nr,'
+        print('    unsigned in_nr,')
+    print('    unsigned out_nr,')
     if intype != GENERATE:
-        print '    unsigned restart_index,'
-    print '    void *_out )'
-    print '{'
+        print('    unsigned restart_index,')
+    print('    void *_out )')
+    print('{')
     if intype != GENERATE:
-        print '  const ' + intype + '*in = (const ' + intype + '*)_in;'
-    print '  ' + outtype + ' *out = (' + outtype + '*)_out;'
-    print '  unsigned i, j;'
-    print '  (void)j;'
+        print('  const ' + intype + '*in = (const ' + intype + '*)_in;')
+    print('  ' + outtype + ' *out = (' + outtype + '*)_out;')
+    print('  unsigned i, j;')
+    print('  (void)j;')
 
 def postamble():
-    print '}'
+    print('}')
 
 
 def tris(intype, outtype):
     preamble(intype, outtype, prim='tris')
-    print '  for (i = start, j = 0; j < out_nr; j+=6, i+=3) { '
+    print('  for (i = start, j = 0; j < out_nr; j+=6, i+=3) { ')
     do_tri( intype, outtype, 'out+j',  'i', 'i+1', 'i+2' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 def tristrip(intype, outtype):
     preamble(intype, outtype, prim='tristrip')
-    print '  for (i = start, j = 0; j < out_nr; j+=6, i++) { '
+    print('  for (i = start, j = 0; j < out_nr; j+=6, i++) { ')
     do_tri( intype, outtype, 'out+j',  'i', 'i+1/*+(i&1)*/', 'i+2/*-(i&1)*/' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 def trifan(intype, outtype):
     preamble(intype, outtype, prim='trifan')
-    print '  for (i = start, j = 0; j < out_nr; j+=6, i++) { '
+    print('  for (i = start, j = 0; j < out_nr; j+=6, i++) { ')
     do_tri( intype, outtype, 'out+j',  '0', 'i+1', 'i+2' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 
 def polygon(intype, outtype):
     preamble(intype, outtype, prim='polygon')
-    print '  for (i = start, j = 0; j < out_nr; j+=2, i++) { '
+    print('  for (i = start, j = 0; j < out_nr; j+=2, i++) { ')
     line( intype, outtype, 'out+j', 'i', '(i+1)%(out_nr/2)' )
-    print '   }'
+    print('   }')
     postamble()
 
 
 def quads(intype, outtype):
     preamble(intype, outtype, prim='quads')
-    print '  for (i = start, j = 0; j < out_nr; j+=8, i+=4) { '
+    print('  for (i = start, j = 0; j < out_nr; j+=8, i+=4) { ')
     do_quad( intype, outtype, 'out+j', 'i+0', 'i+1', 'i+2', 'i+3' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 def quadstrip(intype, outtype):
     preamble(intype, outtype, prim='quadstrip')
-    print '  for (i = start, j = 0; j < out_nr; j+=8, i+=2) { '
+    print('  for (i = start, j = 0; j < out_nr; j+=8, i+=2) { ')
     do_quad( intype, outtype, 'out+j', 'i+2', 'i+0', 'i+1', 'i+3' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 def trisadj(intype, outtype):
     preamble(intype, outtype, prim='trisadj')
-    print '  for (i = start, j = 0; j < out_nr; j+=6, i+=6) { '
+    print('  for (i = start, j = 0; j < out_nr; j+=6, i+=6) { ')
     do_tri( intype, outtype, 'out+j',  'i', 'i+2', 'i+4' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 def tristripadj(intype, outtype):
     preamble(intype, outtype, prim='tristripadj')
-    print '  for (i = start, j = 0; j < out_nr; j+=6, i+=2) { '
+    print('  for (i = start, j = 0; j < out_nr; j+=6, i+=2) { ')
     do_tri( intype, outtype, 'out+j',  'i', 'i+2', 'i+4' );
-    print '   }'
+    print('   }')
     postamble()
 
 
@@ -227,16 +229,16 @@ def emit_funcs():
 
 def init(intype, outtype, prim):
     if intype == GENERATE:
-        print ('generate_line[' + 
+        print(('generate_line[' + 
                outtype_idx[outtype] + 
                '][' + longprim[prim] + 
-               '] = ' + name( intype, outtype, prim ) + ';')
+               '] = ' + name( intype, outtype, prim ) + ';'))
     else:
-        print ('translate_line[' + 
+        print(('translate_line[' + 
                intype_idx[intype] + 
                '][' + outtype_idx[outtype] + 
                '][' + longprim[prim] + 
-               '] = ' + name( intype, outtype, prim ) + ';')
+               '] = ' + name( intype, outtype, prim ) + ';'))
 
 
 def emit_all_inits():
@@ -246,19 +248,19 @@ def emit_all_inits():
                 init(intype, outtype, prim)
 
 def emit_init():
-    print 'void u_unfilled_init( void )'
-    print '{'
-    print '  static int firsttime = 1;'
-    print '  if (!firsttime) return;'
-    print '  firsttime = 0;'
+    print('void u_unfilled_init( void )')
+    print('{')
+    print('  static int firsttime = 1;')
+    print('  if (!firsttime) return;')
+    print('  firsttime = 0;')
     emit_all_inits()
-    print '}'
+    print('}')
 
 
     
 
 def epilog():
-    print '#include "indices/u_unfilled_indices.c"'
+    print('#include "indices/u_unfilled_indices.c"')
 
 
 def main():

src/mapi/glapi/gen/gl_enums.py
@@ -25,6 +25,8 @@
 # Authors:
 #    Zack Rusin <zack@kde.org>
 
+from __future__ import print_function
+
 import argparse
 
 import license
@@ -48,20 +50,20 @@ class PrintGlEnums(gl_XML.gl_print_base):
 
 
     def printRealHeader(self):
-        print '#include "main/glheader.h"'
-        print '#include "main/enums.h"'
-        print '#include "main/imports.h"'
-        print '#include "main/mtypes.h"'
-        print ''
-        print 'typedef struct PACKED {'
-        print '   uint32_t offset;'
-        print '   int n;'
-        print '} enum_elt;'
-        print ''
+        print('#include "main/glheader.h"')
+        print('#include "main/enums.h"')
+        print('#include "main/imports.h"')
+        print('#include "main/mtypes.h"')
+        print('')
+        print('typedef struct PACKED {')
+        print('   uint32_t offset;')
+        print('   int n;')
+        print('} enum_elt;')
+        print('')
         return
 
     def print_code(self):
-        print """
+        print("""
 typedef int (*cfunc)(const void *, const void *);
 
 /**
@@ -144,7 +146,7 @@ _mesa_lookup_prim_by_nr(GLuint nr)
 }
 
 
-"""
+""")
         return
 
 
@@ -154,37 +156,37 @@ _mesa_lookup_prim_by_nr(GLuint nr)
         sorted_enum_values = sorted(self.enum_table.keys())
         string_offsets = {}
         i = 0;
-        print '#if defined(__GNUC__)'
-        print '# define LONGSTRING __extension__'
-        print '#else'
-        print '# define LONGSTRING'
-        print '#endif'
-        print ''
-        print 'LONGSTRING static const char enum_string_table[] = {'
+        print('#if defined(__GNUC__)')
+        print('# define LONGSTRING __extension__')
+        print('#else')
+        print('# define LONGSTRING')
+        print('#endif')
+        print('')
+        print('LONGSTRING static const char enum_string_table[] = {')
         # We express the very long concatenation of enum strings as an array
         # of characters rather than as a string literal to work-around MSVC's
         # 65535 character limit.
         for enum in sorted_enum_values:
             (name, pri) = self.enum_table[enum]
-            print "  ",
+            print("  ", end=' ')
             for ch in name:
-                print "'%c'," % ch,
-            print "'\\0',"
+                print("'%c'," % ch, end=' ')
+            print("'\\0',")
 
             string_offsets[ enum ] = i
             i += len(name) + 1
 
-        print '};'
-        print ''
+        print('};')
+        print('')
 
 
-        print 'static const enum_elt enum_string_table_offsets[%u] =' % (len(self.enum_table))
-        print '{'
+        print('static const enum_elt enum_string_table_offsets[%u] =' % (len(self.enum_table)))
+        print('{')
         for enum in sorted_enum_values:
             (name, pri) = self.enum_table[enum]
-            print '   { %5u, 0x%08X }, /* %s */' % (string_offsets[enum], enum, name)
-        print '};'
-        print ''
+            print('   { %5u, 0x%08X }, /* %s */' % (string_offsets[enum], enum, name))
+        print('};')
+        print('')
 
         self.print_code()
         return
@@ -240,7 +242,7 @@ _mesa_lookup_prim_by_nr(GLuint nr)
             # confuse us.  GL_ACTIVE_PROGRAM_EXT is OK to lose because
             # we choose GL_ACTIVE PROGRAM instead.
             if name in self.string_to_int and name != "GL_ACTIVE_PROGRAM_EXT":
-                print "#error Renumbering {0} from {1} to {2}".format(name, self.string_to_int[name], value)
+                print("#error Renumbering {0} from {1} to {2}".format(name, self.string_to_int[name], value))
 
             self.string_to_int[name] = value
 

src/mapi/glapi/gen/remap_helper.py
@@ -23,6 +23,8 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+from __future__ import print_function
+
 import argparse
 
 import license
@@ -66,23 +68,23 @@ class PrintGlRemap(gl_XML.gl_print_base):
 
 
     def printRealHeader(self):
-        print '#include "main/dispatch.h"'
-        print '#include "main/remap.h"'
-        print ''
+        print('#include "main/dispatch.h"')
+        print('#include "main/remap.h"')
+        print('')
         return
 
 
     def printBody(self, api):
         pool_indices = {}
 
-        print '/* this is internal to remap.c */'
-        print '#ifndef need_MESA_remap_table'
-        print '#error Only remap.c should include this file!'
-        print '#endif /* need_MESA_remap_table */'
-        print ''
+        print('/* this is internal to remap.c */')
+        print('#ifndef need_MESA_remap_table')
+        print('#error Only remap.c should include this file!')
+        print('#endif /* need_MESA_remap_table */')
+        print('')
 
-        print ''
-        print 'static const char _mesa_function_pool[] ='
+        print('')
+        print('static const char _mesa_function_pool[] =')
 
         # output string pool
         index = 0;
@@ -100,26 +102,26 @@ class PrintGlRemap(gl_XML.gl_print_base):
             else:
                 comments = "dynamic"
 
-            print '   /* _mesa_function_pool[%d]: %s (%s) */' \
-                            % (index, f.name, comments)
+            print('   /* _mesa_function_pool[%d]: %s (%s) */' \
+                            % (index, f.name, comments))
             for line in spec:
-                print '   "%s\\0"' % line
+                print('   "%s\\0"' % line)
                 index += len(line) + 1
-        print '   ;'
-        print ''
+        print('   ;')
+        print('')
 
-        print '/* these functions need to be remapped */'
-        print 'static const struct gl_function_pool_remap MESA_remap_table_functions[] = {'
+        print('/* these functions need to be remapped */')
+        print('static const struct gl_function_pool_remap MESA_remap_table_functions[] = {')
         # output all functions that need to be remapped
         # iterate by offsets so that they are sorted by remap indices
         for f in api.functionIterateByOffset():
             if not f.assign_offset:
                 continue
-            print '   { %5d, %s_remap_index },' \
-                            % (pool_indices[f], f.name)
-        print '   {    -1, -1 }'
-        print '};'
-        print ''
+            print('   { %5d, %s_remap_index },' \
+                            % (pool_indices[f], f.name))
+        print('   {    -1, -1 }')
+        print('};')
+        print('')
         return
 
 

loqs/mesa

(cherry picked from commit 0f7b18fa0d353aab0a44082b1aca8d8c00df71a7)
 Conflicts:
	src/amd/common/sid_tables.py
	src/amd/vulkan/vk_format_table.py
	src/compiler/nir/nir_builder_opcodes_h.py
	src/compiler/nir/nir_constant_expressions.py
	src/compiler/nir/nir_opcodes_c.py
	src/compiler/nir/nir_opcodes_h.py
	src/compiler/nir/nir_opt_algebraic.py
	src/gallium/auxiliary/indices/u_indices_gen.py
	src/gallium/auxiliary/indices/u_unfilled_gen.py
	src/gallium/auxiliary/util/u_format_pack.py
	src/gallium/auxiliary/util/u_format_srgb.py
	src/gallium/auxiliary/util/u_format_table.py
	src/gallium/drivers/freedreno/ir3/ir3_nir_trig.py
	src/gallium/drivers/r600/egd_tables.py
	src/intel/compiler/brw_nir_trig_workarounds.py
	src/mapi/glapi/gen/glX_proto_common.py
	src/mapi/glapi/gen/glX_proto_send.py
	src/mapi/glapi/gen/glX_proto_size.py
	src/mapi/glapi/gen/gl_SPARC_asm.py
	src/mapi/glapi/gen/gl_XML.py
	src/mapi/glapi/gen/gl_apitemp.py
	src/mapi/glapi/gen/gl_enums.py
	src/mapi/glapi/gen/gl_genexec.py
	src/mapi/glapi/gen/gl_gentable.py
	src/mapi/glapi/gen/gl_marshal.py
	src/mapi/glapi/gen/gl_marshal_h.py
	src/mapi/glapi/gen/gl_procs.py
	src/mapi/glapi/gen/gl_table.py
	src/mapi/glapi/gen/gl_x86-64_asm.py
	src/mapi/glapi/gen/gl_x86_asm.py
	src/mapi/glapi/gen/remap_helper.py
	src/mapi/glapi/gen/typeexpr.py
	src/mesa/main/format_info.py
	src/mesa/main/format_pack.py
	src/mesa/main/format_unpack.py
	src/mesa/main/get_hash_generator.py
Added
	src/gallium/auxiliary/util/u_half.py
	src/gallium/drivers/llvmpipe/lp_tile_soa.py
	src/gallium/drivers/svga/svgadump/svga_dump.py
	src/glsl/builtins/tools/generate_builtins.py
	src/glsl/builtins/tools/generate_matrixCompMultGLSL.py
	src/glsl/builtins/tools/generate_outerProductGLSL.py
	src/glsl/builtins/tools/generate_transposeGLSL.py
	src/glsl/builtins/tools/texture_builtins.py
	src/mapi/glapi/gen-es/gl_compare.py
	src/mapi/glapi/gen-es/gl_parse_header.py
	src/mapi/glapi/gen/extension_helper.py
	src/mapi/glapi/gen/glX_doc.py
	src/mapi/glapi/gen/glX_pr

src/gallium/auxiliary/indices/u_unfilled_gen.py
@@ -25,6 +25,8 @@ copyright = '''
  */
 '''
 
+from __future__ import print_function
+
 GENERATE, UBYTE, USHORT, UINT = 'generate', 'ubyte', 'ushort', 'uint'
 FIRST, LAST = 'first', 'last'
 
@@ -50,9 +52,9 @@ outtype_idx = dict(ushort='OUT_USHORT', uint='OUT_UINT')
 
 
 def prolog():
-    print '''/* File automatically generated by u_unfilled_gen.py */'''
-    print copyright
-    print r'''
+    print('''/* File automatically generated by u_unfilled_gen.py */''')
+    print(copyright)
+    print(r'''
 
 /**
  * @file
@@ -90,7 +92,7 @@ static unsigned in_size_idx( unsigned index_size )
 static u_generate_func generate_line[OUT_COUNT][PRIM_COUNT];
 static u_translate_func translate_line[IN_COUNT][OUT_COUNT][PRIM_COUNT];
 
-'''
+''')
 
 def vert( intype, outtype, v0 ):
     if intype == GENERATE:
@@ -99,8 +101,8 @@ def vert( intype, outtype, v0 ):
         return '(' + outtype + ')in[' + v0 + ']'
 
 def line( intype, outtype, ptr, v0, v1 ):
-    print '      (' + ptr + ')[0] = ' + vert( intype, outtype, v0 ) + ';'
-    print '      (' + ptr + ')[1] = ' + vert( intype, outtype, v1 ) + ';'
+    print('      (' + ptr + ')[0] = ' + vert( intype, outtype, v0 ) + ';')
+    print('      (' + ptr + ')[1] = ' + vert( intype, outtype, v1 ) + ';')
 
 # XXX: have the opportunity here to avoid over-drawing shared lines in
 # tristrips, fans, etc, by integrating this into the calling functions
@@ -124,68 +126,68 @@ def name(intype, outtype, prim):
         return 'translate_' + prim + '_' + intype + '2' + outtype
 
 def preamble(intype, outtype, prim):
-    print 'static void ' + name( intype, outtype, prim ) + '('
+    print('static void ' + name( intype, outtype, prim ) + '(')
     if intype != GENERATE:
-        print '    const void * _in,'
-    print '    unsigned nr,'
-    print '    void *_out )'
-    print '{'
+        print('    const void * _in,')
+    print('    unsigned nr,')
+    print('    void *_out )')
+    print('{')
     if intype != GENERATE:
-        print '  const ' + intype + '*in = (const ' + intype + '*)_in;'
-    print '  ' + outtype + ' *out = (' + outtype + '*)_out;'
-    print '  unsigned i, j;'
-    print '  (void)j;'
+        print('  const ' + intype + '*in = (const ' + intype + '*)_in;')
+    print('  ' + outtype + ' *out = (' + outtype + '*)_out;')
+    print('  unsigned i, j;')
+    print('  (void)j;')
 
 def postamble():
-    print '}'
+    print('}')
 
 
 def tris(intype, outtype):
     preamble(intype, outtype, prim='tris')
-    print '  for (j = i = 0; j < nr; j+=6, i+=3) { '
+    print('  for (j = i = 0; j < nr; j+=6, i+=3) { ')
     do_tri( intype, outtype, 'out+j',  'i', 'i+1', 'i+2' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 def tristrip(intype, outtype):
     preamble(intype, outtype, prim='tristrip')
-    print '  for (j = i = 0; j < nr; j+=6, i++) { '
+    print('  for (j = i = 0; j < nr; j+=6, i++) { ')
     do_tri( intype, outtype, 'out+j',  'i', 'i+1/*+(i&1)*/', 'i+2/*-(i&1)*/' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 def trifan(intype, outtype):
     preamble(intype, outtype, prim='trifan')
-    print '  for (j = i = 0; j < nr; j+=6, i++) { '
+    print('  for (j = i = 0; j < nr; j+=6, i++) { ')
     do_tri( intype, outtype, 'out+j',  '0', 'i+1', 'i+2' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 
 def polygon(intype, outtype):
     preamble(intype, outtype, prim='polygon')
-    print '  for (j = i = 0; j < nr; j+=2, i++) { '
+    print('  for (j = i = 0; j < nr; j+=2, i++) { ')
     line( intype, outtype, 'out+j', 'i', '(i+1)%(nr/2)' )
-    print '   }'
+    print('   }')
     postamble()
 
 
 def quads(intype, outtype):
     preamble(intype, outtype, prim='quads')
-    print '  for (j = i = 0; j < nr; j+=8, i+=4) { '
+    print('  for (j = i = 0; j < nr; j+=8, i+=4) { ')
     do_quad( intype, outtype, 'out+j', 'i+0', 'i+1', 'i+2', 'i+3' );
-    print '   }'
+    print('   }')
     postamble()
 
 
 def quadstrip(intype, outtype):
     preamble(intype, outtype, prim='quadstrip')
-    print '  for (j = i = 0; j < nr; j+=8, i+=2) { '
+    print('  for (j = i = 0; j < nr; j+=8, i+=2) { ')
     do_quad( intype, outtype, 'out+j', 'i+2', 'i+0', 'i+1', 'i+3' );
-    print '   }'
+    print('   }')
     postamble()
 
 
@@ -201,16 +203,16 @@ def emit_funcs():
 
 def init(intype, outtype, prim):
     if intype == GENERATE:
-        print ('generate_line[' + 
+        print(('generate_line[' + 
                outtype_idx[outtype] + 
                '][' + longprim[prim] + 
-               '] = ' + name( intype, outtype, prim ) + ';')
+               '] = ' + name( intype, outtype, prim ) + ';'))
     else:
-        print ('translate_line[' + 
+        print(('translate_line[' + 
                intype_idx[intype] + 
                '][' + outtype_idx[outtype] + 
                '][' + longprim[prim] + 
-               '] = ' + name( intype, outtype, prim ) + ';')
+               '] = ' + name( intype, outtype, prim ) + ';'))
 
 
 def emit_all_inits():
@@ -220,19 +222,19 @@ def emit_all_inits():
                 init(intype, outtype, prim)
 
 def emit_init():
-    print 'void u_unfilled_init( void )'
-    print '{'
-    print '  static int firsttime = 1;'
-    print '  if (!firsttime) return;'
-    print '  firsttime = 0;'
+    print('void u_unfilled_init( void )')
+    print('{')
+    print('  static int firsttime = 1;')
+    print('  if (!firsttime) return;')
+    print('  firsttime = 0;')
     emit_all_inits()
-    print '}'
+    print('}')
 
 
     
 
 def epilog():
-    print '#include "indices/u_unfilled_indices.c"'
+    print('#include "indices/u_unfilled_indices.c"')
 
 
 def main():

src/mapi/glapi/gen/gl_enums.py
@@ -25,6 +25,8 @@
 # Authors:
 #    Zack Rusin <zack@kde.org>
 
+from __future__ import print_function
+
 import license
 import gl_XML
 import sys, getopt
@@ -41,21 +43,21 @@ class PrintGlEnums(gl_XML.gl_print_base):
 
 
 	def printRealHeader(self):
-		print '#include "main/glheader.h"'
-		print '#include "main/mfeatures.h"'
-		print '#include "main/enums.h"'
-		print '#include "main/imports.h"'
-		print '#include "main/mtypes.h"'
-		print ''
-		print 'typedef struct {'
-		print '   size_t offset;'
-		print '   int n;'
-		print '} enum_elt;'
-		print ''
+		print('#include "main/glheader.h"')
+		print('#include "main/mfeatures.h"')
+		print('#include "main/enums.h"')
+		print('#include "main/imports.h"')
+		print('#include "main/mtypes.h"')
+		print('')
+		print('typedef struct {')
+		print('   size_t offset;')
+		print('   int n;')
+		print('} enum_elt;')
+		print('')
 		return
 
 	def print_code(self):
-		print """
+		print("""
 typedef int (*cfunc)(const void *, const void *);
 
 /**
@@ -158,7 +160,7 @@ int _mesa_lookup_enum_by_name( const char *symbol )
    return (f != NULL) ? f->n : -1;
 }
 
-"""
+""")
 		return
 
 
@@ -187,34 +189,34 @@ int _mesa_lookup_enum_by_name( const char *symbol )
 
 		string_offsets = {}
 		i = 0;
-		print 'LONGSTRING static const char enum_string_table[] = '
+		print('LONGSTRING static const char enum_string_table[] = '
 		for [name, enum] in name_table:
-			print '   "%s\\0"' % (name)
+			print('   "%s\\0"' % (name))
 			string_offsets[ name ] = i
 			i += len(name) + 1
 
-		print '   ;'
-		print ''
+		print('   ;')
+		print('')
 
 
-		print 'static const enum_elt all_enums[%u] =' % (len(name_table))
-		print '{'
+		print('static const enum_elt all_enums[%u] =' % (len(name_table)))
+		print('{')
 		for [name, enum] in name_table:
-			print '   { %5u, 0x%08X }, /* %s */' % (string_offsets[name], enum, name)
-		print '};'
-		print ''
+			print('   { %5u, 0x%08X }, /* %s */' % (string_offsets[name], enum, name))
+		print('};')
+		print('')
 
-		print 'static const unsigned reduced_enums[%u] =' % (len(keys))
-		print '{'
+		print('static const unsigned reduced_enums[%u] =' % (len(keys)))
+		print('{')
 		for enum in keys:
 			name = enum_table[ enum ]
 			if [name, enum] not in name_table:
-				print '      /* Error! %s, 0x%04x */ 0,' % (name, enum)
+				print('      /* Error! %s, 0x%04x */ 0,' % (name, enum))
 			else:
 				i = name_table.index( [name, enum] )
 
-				print '      %4u, /* %s */' % (i, name)
-		print '};'
+				print('      %4u, /* %s */' % (i, name))
+		print('};')
 
 
 		self.print_code()
@@ -239,7 +241,7 @@ int _mesa_lookup_enum_by_name( const char *symbol )
 
 
 def show_usage():
-	print "Usage: %s [-f input_file_name]" % sys.argv[0]
+	print("Usage: %s [-f input_file_name]" % sys.argv[0])
 	sys.exit(1)
 
 if __name__ == '__main__':

src/mapi/glapi/gen/remap_helper.py
@@ -23,6 +23,8 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+from __future__ import print_function
+
 import gl_XML
 import license
 import sys, getopt, string
@@ -63,19 +65,19 @@ class PrintGlRemap(gl_XML.gl_print_base):
 
 
 	def printRealHeader(self):
-		print '#include "main/dispatch.h"'
-		print '#include "main/remap.h"'
-		print ''
+		print('#include "main/dispatch.h"')
+		print('#include "main/remap.h"')
+		print('')
 		return
 
 
 	def printBody(self, api):
 		pool_indices = {}
 
-		print '/* this is internal to remap.c */'
-		print '#ifdef need_MESA_remap_table'
-		print ''
-		print 'static const char _mesa_function_pool[] ='
+		print('/* this is internal to remap.c */')
+		print('#ifdef need_MESA_remap_table')
+		print('')
+		print('static const char _mesa_function_pool[] =')
 
 		# output string pool
 		index = 0;
@@ -93,26 +95,26 @@ class PrintGlRemap(gl_XML.gl_print_base):
 			else:
 				comments = "dynamic"
 
-			print '   /* _mesa_function_pool[%d]: %s (%s) */' \
-					% (index, f.name, comments)
+			print('   /* _mesa_function_pool[%d]: %s (%s) */' \
+					% (index, f.name, comments))
 			for line in spec:
-				print '   "%s\\0"' % line
+				print('   "%s\\0"' % line)
 				index += len(line) + 1
-		print '   ;'
-		print ''
+		print('   ;')
+		print('')
 
-		print '/* these functions need to be remapped */'
-		print 'static const struct gl_function_pool_remap MESA_remap_table_functions[] = {'
+		print('/* these functions need to be remapped */')
+		print('static const struct gl_function_pool_remap MESA_remap_table_functions[] = {')
 		# output all functions that need to be remapped
 		# iterate by offsets so that they are sorted by remap indices
 		for f in api.functionIterateByOffset():
 			if not f.assign_offset:
 				continue
-			print '   { %5d, %s_remap_index },' \
-					% (pool_indices[f], f.name)
-		print '   {    -1, -1 }'
-		print '};'
-		print ''
+			print('   { %5d, %s_remap_index },' \
+					% (pool_indices[f], f.name))
+		print('   {    -1, -1 }')
+		print('};')
+		print('')
 
 		# collect functions by versions/extensions
 		extension_functions = {}
@@ -138,8 +140,8 @@ class PrintGlRemap(gl_XML.gl_print_base):
 		extensions.sort()
 
 		# output ABI functions that have alternative names (with ext suffix)
-		print '/* these functions are in the ABI, but have alternative names */'
-		print 'static const struct gl_function_remap MESA_alt_functions[] = {'
+		print('/* these functions are in the ABI, but have alternative names */')
+		print('static const struct gl_function_remap MESA_alt_functions[] = {')
 		for ext in extensions:
 			funcs = []
 			for f in extension_functions[ext]:
@@ -148,16 +150,16 @@ class PrintGlRemap(gl_XML.gl_print_base):
 					funcs.append(f)
 			if not funcs:
 				continue
-			print '   /* from %s */' % ext
+			print('   /* from %s */' % ext)
 			for f in funcs:
-				print '   { %5d, _gloffset_%s },' \
-						% (pool_indices[f], f.name)
-		print '   {    -1, -1 }'
-		print '};'
-		print ''
+				print('   { %5d, _gloffset_%s },' \
+						% (pool_indices[f], f.name))
+		print('   {    -1, -1 }')
+		print('};')
+		print('')
 
-		print '#endif /* need_MESA_remap_table */'
-		print ''
+		print('#endif /* need_MESA_remap_table */')
+		print('')
 
 		# output remap helpers for DRI drivers
 
@@ -173,30 +175,30 @@ class PrintGlRemap(gl_XML.gl_print_base):
 					# abi, or have offset -1
 					funcs.append(f)
 
-			print '#if defined(need_%s)' % (ext)
+			print('#if defined(need_%s)' % (ext))
 			if remapped:
-				print '/* functions defined in MESA_remap_table_functions are excluded */'
+				print('/* functions defined in MESA_remap_table_functions are excluded */')
 
 			# output extension functions that need to be mapped
-			print 'static const struct gl_function_remap %s_functions[] = {' % (ext)
+			print('static const struct gl_function_remap %s_functions[] = {' % (ext))
 			for f in funcs:
 				if f.offset >= 0:
-					print '   { %5d, _gloffset_%s },' \
-							% (pool_indices[f], f.name)
+					print('   { %5d, _gloffset_%s },' \
+							% (pool_indices[f], f.name))
 				else:
-					print '   { %5d, -1 }, /* %s */' % \
-							(pool_indices[f], f.name)
-			print '   {    -1, -1 }'
-			print '};'
+					print('   { %5d, -1 }, /* %s */' % \
+							(pool_indices[f], f.name))
+			print('   {    -1, -1 }')
+			print('};')
 
-			print '#endif'
-			print ''
+			print('#endif')
+			print('')
 
 		return
 
 
 def show_usage():
-	print "Usage: %s [-f input_file_name]" % sys.argv[0]
+	print("Usage: %s [-f input_file_name]" % sys.argv[0])
 	sys.exit(1)
 
 if __name__ == '__main__':